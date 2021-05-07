d22b4eb9e377f50734b169e1dfb37aac18713d61
daophanba <83689481+daophanba@users.noreply.github.com>
2021-05-07 12:28:33 +0700

    tính luỹ thừa

---
+++ giai thua/ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Loop
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("nhap mot so bat ky");
+			int n = int.Parse(Console.ReadLine());
+			int answer = 1;
+			for (int i = 1; i <= n; i++)
+			{
+				// answer *= i means answer = answer * i;
+				answer *= i;
+			}
+			Console.Write(answer);
+		}
+	}
+}
